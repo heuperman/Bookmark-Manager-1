7b724c3b569cd94c770bf3e8988283d45544304e
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-06 12:01:55 +0100

    Add test for comment feature

---
+++ spec/features/add_comment_spec.rb
@@ -0,0 +1,12 @@
+feature 'add comment' do
+  scenario 'adding a comment to a bookmark' do
+    visit('/')
+    fill_in('title', with: 'Facebook')
+    fill_in('url', with: 'http://facebook.com')
+    click_button('Add bookmark')
+    click_button('Add comment', match: :first)
+    fill_in('comment', with: 'This is a comment.')
+    click_button('Submit')
+    expect(page).to have_content 'This is a comment'
+  end
+end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,5 +2,5 @@ require 'pg'
 
 def setup_test_database
   connection = PG.connect(dbname: 'bookmark_manager_test')
-  connection.exec('TRUNCATE TABLE bookmarks')
+  connection.exec('TRUNCATE TABLE comments, bookmarks, tags, bookmark_tags;')
 end
